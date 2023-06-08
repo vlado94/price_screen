ad88de036c8103efe1e7962ab954ca869d9c72b4
sasamijat <sasasasamijatmijat@gmail.com>
2023-06-08 18:30:15 +0200

    A

--- PriceScreen/PriceScreen/Models/AddStoreViewModel.cs
+++ PriceScreen/PriceScreen/Models/AddStoreViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace PriceScreen.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceScreen.Models
 {
     public class AddStoreViewModel
     {
         public string StoreName { get; set; }
         public string StoreLocation { get; set; }
+
+        [Display(Name = "Image")]
+        public string ProfilePicture { get; set; }
     }
 }
--- PriceScreen/PriceScreen/Models/Store.cs
+++ PriceScreen/PriceScreen/Models/Store.cs
@@ -1,12 +1,15 @@
-﻿namespace PriceScreen.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceScreen.Models
 {
     public class Store
     {
         public int Id { get; set; }
         public string StoreName { get; set; }
         public string StoreLocation { get; set; }
-        
 
+        [Display(Name = "Image")]
+        public string ProfilePicture { get; set; }
 
     }
 }
