a0c3122ed11463a8b631544b3005700d4639e569
sasamijat <sasasasamijatmijat@gmail.com>
2023-06-08 15:33:10 +0200

    DodavanjeProdavnice

--- PriceScreen/PriceScreen/Controllers/StoreController.cs
+++ PriceScreen/PriceScreen/Controllers/StoreController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PriceScreen.Data;
 using PriceScreen.Models;
 
 namespace PriceScreen.Controllers
 {
     public class StoreController : Controller
     {
+        private readonly PriceScreenDbContext pscontext;
+        public StoreController(PriceScreenDbContext pscontext)
+        {
+            this.pscontext = pscontext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Add(AddStoreViewModel addStoreRequest)
+        {
+            var store = new Store()
+            {
+                //Id = Guid.NewGuid(),
+                StoreName = addStoreRequest.StoreName,
+                StoreLocation = addStoreRequest.StoreLocation
+            };
+
+            await pscontext.Stores.AddAsync(store);
+            await pscontext.SaveChangesAsync();
+            return RedirectToAction("Add");
         }
 
     }
--- PriceScreen/PriceScreen/Models/Store.cs
+++ PriceScreen/PriceScreen/Models/Store.cs
@@ -2,7 +2,7 @@
 {
     public class Store
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string StoreName { get; set; }
         public string StoreLocation { get; set; }
         
